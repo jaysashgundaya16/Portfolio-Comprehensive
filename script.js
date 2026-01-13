b48f678e77b9bf2736b5a3d0e5983ff49e21af7f
daciple-matthew <daciplerosemateo@gmail.com>
2026-01-13 12:54:36 +0800

    added layout and designs for css

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-function changeText() {
-    let name = document.getElementById("name");
-    name.textContent = "Welcome to My Portfolio!";
+class PortfolioApp {
+    constructor() {
+        this.name = "Jaysash Gundaya";
+        this.roles = [
+            "Future Software Developer",
+            "Web Developer",
+            "Java Programmer",
+            "IoT System Builder"
+        ];
+        this.nameIndex = 0;
+        this.roleIndex = 0;
+
+        this.typeName();
+        this.rotateRole();
+    }
+
+    typeName() {
+        if (this.nameIndex < this.name.length) {
+            document.getElementById("typing-name").textContent +=
+                this.name.charAt(this.nameIndex);
+            this.nameIndex++;
+            setTimeout(() => this.typeName(), 120);
+        }
+    }
+
+    rotateRole() {
+        document.getElementById("role").textContent =
+            this.roles[this.roleIndex];
+        this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+        setTimeout(() => this.rotateRole(), 2500);
+    }
+
+    toggleDark() {
+        document.body.classList.toggle("dark");
+    }
 }
+
+const app = new PortfolioApp();
